2720000318923c83b9434870c921e43ba6875ec3
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2019-11-22 19:05:08 +0200

    added action acons for user items

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,99 +1,132 @@
 <template>
-	<ul class="user-list">
-		<li class="user-list__item" v-for="user in users" :key="user.id">
-			<div class="user-info">
-				<div class="user-info__name">
-					{{user.lName}} {{user.fName}} {{user.sName}}
-				</div>
-				<div class="user-info__phone">
-					699-95-39
-				</div>
-			</div>
-		</li>
-	</ul>
+  <ul class="user-list">
+    <li class="user-list__item" v-for="user in users" :key="user.id">
+      <div class="user-info">
+        <div class="user-info__name">
+          {{user.lName}} {{user.fName}} {{user.sName}}
+        </div>
+        <div class="user-info__phone">
+          699-95-39
+        </div>
+        <div class="user-info__actions" @click="doAction">
+          <router-link to="/about"><i-edit class="icon"></i-edit></router-link>
+          <a href="#"><i-remove class="icon"></i-remove></a>
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
 
 import users from './users.json';
+import iEdit from '@/assets/icons/edit.svg';
+import iRemove from '@/assets/icons/remove.svg';
 
 export default {
-	name: 'home',
+  name: 'home',
 
-	data() {
-		return {
-			users: users
-		}
-	},
+  components: {
+    iEdit,
+    iRemove,
+  },
 
-	mounted() {
-		console.log(users);
-	},
+  data() {
+    return {
+      users: users
+    }
+  },
+
+  mounted() {
+    console.log(users);
+  },
+
+  methods: {
+    doAction(e) {
+      console.log(e.target, e.currentTarget);
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.user-list__item {
-	//background: #066;
-	border-bottom: 1px solid #606060;
-	//border-right: 4px solid $color_secondary;
-	width: 100%;
-	height: 112px;
-	padding: 0 16px;
-	position: relative;
-	overflow: hidden;
-	cursor: pointer;
 
-	&:hover {
-		//border-right: 4px solid $color_secondary;
-		//background: $bg-active;
-		// &:after {
-		// 	content: '';
-		// 	background: $color_secondary;
-		// 	display: block;
-		// 	width: 12px;
-		// 	height: 12px;
-		// 	position: absolute;
-		// 	right: 0;
-		// 	top: 50%;
-		// 	transform: translate(50%, -50%) rotate(45deg);
-		//}
-		.name {
-			//color: $color_primary_active;
-		}
-		.phone {
-			//color: #369;
-		}
-	}
+$list-item-hover: $md-blue-grey-700;
 
-	&:last-child {
-		border-bottom: none;
-	}
-}
+.user-list {
+  margin: 0 -1rem;
 
-.user-info {
-	//background: #066;
-	//height: 80px;
-	//margin: 16px 0 0 8px;
-	//float: left;
+  &__item {
+    width: 100%;
+    padding: 0 1rem;
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
 
-	&__name {
-		font: 24px Helvetica;
-		color: $color_primary;
-		letter-spacing: 1px;
-		height: 64px;
-		line-height: 64px;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
+    &:not(:last-child) .user-info {
+      border-bottom: 1px solid $md-blue-grey-500;
+    }
+  }
 
-	&__phone {
-		font-size: 18px;
-		color: $color_secondary;
-		height: 32px;
-		line-height: 32px;
-		letter-spacing: 1px;
-	}
+  .user-info {
+    padding: .5rem 0;
+    //margin-right: 4rem;
+    transition: padding .2s;
+    position: relative;
+    overflow: hidden;
+    //background: #099;
+
+    &:hover {
+      background: $list-item-hover;
+      padding-left: 1rem;
+
+      .user-info__actions {
+        opacity: 1;
+        top: 0;
+      }
+    }
+
+    &__name {
+      font: 24px Helvetica;
+      color: $color_primary;
+      letter-spacing: 1px;
+      padding: 0.5rem 0;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    &__phone {
+      font-size: 18px;
+      color: $color-secondary;
+      letter-spacing: 1px;
+      padding: 0.25rem 0;
+    }
+
+    &__actions {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 3rem;
+      height: 100%;
+      position: absolute;
+      right: 0;
+      top: -2rem;
+      //top: 0;
+      opacity: 0;
+      transition: top .2s, opacity .2s;
+
+      .icon {
+        fill: #fff;
+        margin: .25rem 0;
+        opacity: 0.5;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
 }
 </style>

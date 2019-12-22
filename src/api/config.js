972aecb6ed18e6ca193e0c51d45a16b6fbd80bb2
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2019-12-22 19:21:03 +0200

    added 'db.json' file

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { flags } from '@/defs/flags';
 
-axios.defaults.baseURL = 'https://my-json-server.typicode.com/alexander-lyakhov/epam-tz';
+axios.defaults.baseURL = 'https://my-json-server.typicode.com/alexander-lyakhov/vue-contacts';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 axios.interceptors.request.use(
